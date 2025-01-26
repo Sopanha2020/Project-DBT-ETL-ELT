29b770a0342e0295d8fa98a6aa8a79a2a9374a6f
Sopanha2020 <62559056+Sopanha2020@users.noreply.github.com>
2025-01-26 20:39:24 +0100

    Delete target/compiled/dbt1 directory

--- target/compiled/dbt1/models/staging/stg_customers_status.sql
+++
@@ -1,6 +0,0 @@
-with status_data as (
-    select *
-    from `my_dbt_db`.`customer_status`
-)
-select *
-from status_data
--- target/compiled/dbt1/tests/order_dates_in_the_past.sql
+++
@@ -1,8 +0,0 @@
-with invalid_orders as (
-    select *
-    from `my_dbt_db`.`stg_orders`
-    where order_date > current_date
-)
-select count(*)
-from invalid_orders
-having count(*) > 0
